61c938b5aaccafd686fe9e57f04e8ad149b3d532
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-19 22:52:32 -0300

    Excluindo recursos.

--- src/main/java/com/lambdasys/java/kubernetes/persistence/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lambdasys.java.kubernetes.persistence;
-
-import com.lambdasys.java.kubernetes.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
-
